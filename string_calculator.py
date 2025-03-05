4f60b1acc029fe468b780fa8967eb88f75a6aff1
Anjitha Devan <anjithadevan@gmail.com>
2025-03-05 23:55:53 +0530

    added logic to handle new lines between numbers

--- string_calculator.py
+++ string_calculator.py
@@ -1,11 +1,14 @@
-input_data = input("enter the number string comma separated : ")
+import re
+
+#Allow the add method to handle new lines between numbers
+input_data = input("enter the number string : ")
 
 
 def calculate_sum(input_data):
-    data_list = input_data.split(',')
+    data_list = [int(num) for num in re.findall(r'\d+', input_data.replace("\n", " "))]
     sum_numbers = 0
     for num in data_list:
-        sum_numbers = sum_numbers + int(num)
+        sum_numbers = sum_numbers + num
     return sum_numbers
 
 
