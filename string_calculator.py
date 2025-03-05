1fb286d211f11aa67a9866e354c6ac3a13b87f48
Anjitha Devan <anjithadevan@gmail.com>
2025-03-05 23:58:29 +0530

    changed code to Support different delimiters

--- string_calculator.py
+++ string_calculator.py
@@ -1,11 +1,11 @@
 import re
 
-#Allow the add method to handle new lines between numbers
+#Support different delimiters
 input_data = input("enter the number string : ")
 
 
 def calculate_sum(input_data):
-    data_list = [int(num) for num in re.findall(r'\d+', input_data.replace("\n", " "))]
+    data_list = [int(num) for num in re.findall(r'\d+', input_data)]
     sum_numbers = 0
     for num in data_list:
         sum_numbers = sum_numbers + num
